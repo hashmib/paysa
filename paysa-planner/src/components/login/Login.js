ecb44553c6d98f0ba44dc7ac24b9f647f57d980f
Adeel Shaikh <adeel.shaikh@wustl.edu>
2020-07-23 02:08:30 -0500

    changed Login, using hooks, handlers

--- paysa-planner/src/components/login/Login.js
+++ paysa-planner/src/components/login/Login.js
@@ -1,51 +1,151 @@
-import React from 'react';  
-import Button from '@material-ui/core/Button';  
-import TextField from '@material-ui/core/TextField';  
-import Grid from '@material-ui/core/Grid';  
-import Typography from '@material-ui/core/Typography';  
+import React, {useState, useEffect, useSelector, useDispatch} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-class Login extends React.Component {  
-   handleLogin = () => {  
-   alert("HANDLE LOGIN FUNCTION");  
-}  
-render() {  
-   return (  
-      <Grid  
-         container  
-         spacing={0}  
-         direction="column"  
-         alignItems="center"  
-         justify="center"  
-         style={{ minHeight: '100vh' }}  
-         >  
-   <Typography component="h1" variant="h5">  
-      Log in  
-   </Typography>  
-   <form onSubmit={this.handleLogin}>  
-   <TextField  
-      variant="outlined"  
-      margin="normal"  
-      fullWidth  
-      label="Email Address"  
-   />  
-   <TextField  
-      variant="outlined"  
-      margin="normal"  
-      fullWidth  
-      label="Password"  
-      type="password"  
-   />  
-   <Button  
-      type="submit"  
-      fullWidth  
-      variant="contained"  
-      color="primary"  
-      >  
-      Log In  
-      </Button>  
-   </form>  
-  </Grid>  
-  );  
- }    
-}  
-export default Login; 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        PaySa
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login() {
+
+  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  
+    //const loggingIn = useSelector(state => state.authentication.loggingIn);
+    //const dispatch = useDispatch();
+
+    // reset login status
+    useEffect(() => { 
+        //dispatch(userActions.logout()); 
+    }, []);
+
+    
+
+    function handleSubmit(e) {
+        const inputs = username + " " + password
+        alert(inputs)
+        console.log(inputs)
+        //e.preventDefault();
+
+        // setSubmitted(true);
+        // if (username && password) {
+        //     dispatch(userActions.login(username, password));
+        // }
+    }
+
+
+    const classes = useStyles();
+
+
+
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
+          <TextField
+            onChange={(e)=>setUsername(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            onChange={(e)=>setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
