cc86b90d61de3ae5aef840e73065a8431bd4a58c
hashmib <hashmi.sbilal@gmail.com>
2020-07-23 12:38:39 -0500

    setting up the backend

---
+++ paysa-planner/src/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+
+let app = express();
+let router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', router);
+
+router.get('/login', (req, res) => {
+    console.log('request received')
+    console.log(req.body); //shows query paramters sent
+    res.send("Hello!"); 
+});
+app.listen('8080', () => console.log('listening on 8080'));
+
