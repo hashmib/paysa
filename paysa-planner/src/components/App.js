208511816e7282b0ff09c98724eb1dcf921510a0
Adeel Shaikh <adeel.shaikh@wustl.edu>
2020-07-23 00:11:21 -0500

    added material ui icons dependancy and Login component

--- paysa-planner/src/components/App.js
+++ paysa-planner/src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../css/App.css';
+import Login from "./Login"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Budget Planner
+          <Login />
         </p>
       </header>
     </div>
