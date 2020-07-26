412fbd540c867e3398fc9c4882317cf58bc49db8
hashmib <hashmi.sbilal@gmail.com>
2020-07-25 23:47:43 -0500

    MongoDB setting up.

--- paysa-planner/src/server/server.js
+++ paysa-planner/src/server/server.js
@@ -1,7 +1,17 @@
-// Sets up the express server
+// Sets up the express server and database
 const app = require('express')();
 const routes = require('./routes');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// Set up DB
+require('dotenv').config()
+mongoose.connect(process.env.USERDATABASE_URL, { useNewUrlParser: true})
+const db = mongoose.connection
+
+// Check DB connected
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to user database'))
 
 // To format in JSON
 app.use(bodyParser.json());
@@ -12,5 +22,3 @@ app.use('/', routes);
 
 // Listen to incoming HTTP requests on port 8080
 app.listen('8080', () => console.log('listening on 8080'));
-
-
