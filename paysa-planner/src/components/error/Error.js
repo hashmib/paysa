2f867972a63cf5e2362f3c27d6474af0303d0b1f
Adeel Shaikh <adeel.shaikh@wustl.edu>
2020-08-02 22:45:22 -0500

    added error.js file

---
+++ paysa-planner/src/components/error/Error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const Error = () => {
+	return (
+		<Typography component="h1" variant="h5">Page Not Found</Typography>
+	)
+}
+
+export default Error;
