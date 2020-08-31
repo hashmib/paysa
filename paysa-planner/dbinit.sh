80a2259a1ff3bc79e60642e731c7e4b807da7894
amfarooq <farooqui.ali97@gmail.com>
2020-08-30 21:37:18 -0700

    added transactions table to dbinit.sh

--- paysa-planner/dbinit.sh
+++ paysa-planner/dbinit.sh
@@ -6,4 +6,16 @@ MYSQL_PASS="Nokrigang2020!"
 # Connect to root's database instance
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASS" paysadb<<EOFMYSQL
 use paysadb;
+
+CREATE TABLE Transactions(
+    id INT AUTO_INCREMENT,
+    amount INT,
+    description VARCHAR(100),
+    expense BOOLEAN,
+    userid INT,
+    time DATETIME,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES users(id)
+);
+show tables;
 EOFMYSQL
