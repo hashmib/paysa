267c007119075b7306af2f56b72fc31ef821689f
Adeel Shaikh <adeel.shaikh@wustl.edu>
2020-09-04 15:51:05 -0500

    cleaner code for days dropdown select

--- paysa-planner/src/components/configure/Configure.js
+++ paysa-planner/src/components/configure/Configure.js
@@ -150,6 +150,7 @@ export default function Configure(props) {
   }
   
   const stepExpenses = () => {
+    const days = Array.from(Array(31), (_, i) => i + 1);
     return (
         <Container component="main" maxWidth="sm">
             <div className={classes.root}>
@@ -183,34 +184,9 @@ export default function Configure(props) {
                   name="date"
                   onChange={(event) => handleExpensesChange(element, index, event)}
                 >
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                  <MenuItem value={4}>4</MenuItem>
-                  <MenuItem value={5}>5</MenuItem>
-                  <MenuItem value={6}>6</MenuItem>
-                  <MenuItem value={7}>7</MenuItem>
-                  <MenuItem value={8}>8</MenuItem>
-                  <MenuItem value={9}>9</MenuItem>
-                  <MenuItem value={10}>10</MenuItem>
-                  <MenuItem value={11}>11</MenuItem>
-                  <MenuItem value={12}>12</MenuItem>
-                  <MenuItem value={13}>13</MenuItem>
-                  <MenuItem value={14}>14</MenuItem>
-                  <MenuItem value={15}>15</MenuItem>
-                  <MenuItem value={16}>16</MenuItem>
-                  <MenuItem value={17}>17</MenuItem>
-                  <MenuItem value={18}>18</MenuItem>
-                  <MenuItem value={19}>19</MenuItem>
-                  <MenuItem value={20}>20</MenuItem>
-                  <MenuItem value={21}>21</MenuItem>
-                  <MenuItem value={22}>22</MenuItem>
-                  <MenuItem value={23}>23</MenuItem>
-                  <MenuItem value={24}>24</MenuItem>
-                  <MenuItem value={25}>25</MenuItem>
-                  <MenuItem value={26}>26</MenuItem>
-                  <MenuItem value={27}>27</MenuItem>
-                  <MenuItem value={28}>28</MenuItem>
+                  {days.map((day) => {
+                    return (<MenuItem value={day}>{day}</MenuItem>)
+                  })}
                 </Select>
               </FormControl>
               </div>
@@ -276,7 +252,7 @@ export default function Configure(props) {
       if(response.data.added) {
         props.history.push({
           pathname: '/index',
-          //search: '?query=abc',
+          //search: '?query=abc',p
           state: { data }
         })
       } else {
