5ec5fe859a29491fa3cc9b81418ae503e7eeb88c
hashmib <hashmi.sbilal@gmail.com>
2020-09-04 01:29:31 -0500

    shell script

--- paysa-planner/dbinit.sh
+++ paysa-planner/dbinit.sh
@@ -3,15 +3,34 @@
 MYSQL_USER="root"
 MYSQL_PASS="Nokrigang2020!"
 
-# Connect to root's database instance
+# Delete database
+if [[ $1 == '-d' ]];
+then
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASS" paysadb<<EOFMYSQL
+DROP DATABASE paysadb;
+EOFMYSQL
+echo "paysa db deleted"
+
+# Initiate database
+elif [[ $1 == '-i' ]];
+then
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASS" <<EOFMYSQL
+CREATE DATABASE paysadb;
 use paysadb;
+
+CREATE TABLE Users(
+    id INT AUTO_INCREMENT,
+    username VARCHAR(50),
+    password VARCHAR(100),
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE Transactions(
     id INT AUTO_INCREMENT,
     amount INT,
     description VARCHAR(100),
     expense BOOLEAN,
+    month INT,
     userid INT,
     time DATETIME,
     PRIMARY KEY (id),
@@ -19,3 +38,6 @@ CREATE TABLE Transactions(
 );
 show tables;
 EOFMYSQL
+else
+echo "Badbadnotgood"
+fi
