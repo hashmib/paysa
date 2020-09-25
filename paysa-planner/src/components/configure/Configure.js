080121dc640ae32620b3cf0fa3569eb5680474ce
Adeel Shaikh <adeel.shaikh@wustl.edu>
2020-09-25 14:41:20 -0500

    minor code cleaning in configure

--- paysa-planner/src/components/configure/Configure.js
+++ paysa-planner/src/components/configure/Configure.js
@@ -3,7 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import React, {Fragment, useState} from 'react';
+import React, { Fragment, useState } from 'react';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
@@ -47,79 +47,75 @@ const useStyles = makeStyles((theme) => ({
 
 function getSteps() {
   return ['Monthly Income', 'Expenses', 'Complete'];
-}
+};
 
 export default function Configure(props) {
   const classes = useStyles();
   // <--------------------------- Monthly Income ---------------------->
   const [incomes, setIncomes] = useState(
-    [{value: "", label: "", start: new Date(), end: new Date(), frequency: ""}]
+    [{ value: "", label: "", start: new Date(), end: new Date(), frequency: "" }]
   );
   const addIncomesClick = () => {
     setIncomes(incomes => (
-    	[...incomes, { value: "", label: "", start: new Date(), end: new Date(), frequency: ""}]
+      [...incomes, { value: "", label: "", start: new Date(), end: new Date(), frequency: "" }]
     ))
   };
   const removeIncomesClick = (element, index) => {
     let currentIncomes = [...incomes];
     currentIncomes.splice(index, 1);
     setIncomes(currentIncomes)
-  }
+  };
   const handleIncomesChange = (element, index, event) => {
     let currentIncomes = [...incomes]
-    currentIncomes[index] = {...currentIncomes[index], [event.target.name]: event.target.value}
+    currentIncomes[index] = { ...currentIncomes[index], [event.target.name]: event.target.value }
     setIncomes(currentIncomes)
   };
   // <--------------------------- Expenses ---------------------------->
   const [expenses, setExpenses] = useState(
-    [{value: "", 
-      label: "", 
-      start: new Date(), 
-      end: new Date(), 
-      frequency: ""}]
-  )
+    [{ value: "", label: "", start: new Date(), end: new Date(), frequency: ""}]
+  );
   const addExpensesClick = () => {
     setExpenses(expenses => (
-    	[...expenses, { value: "", label: "", start: new Date(), end: new Date(), frequency: ""}]
+      [...expenses, { value: "", label: "", start: new Date(), end: new Date(), frequency: "" }]
     ))
-  }
+  };
   const removeExpensesClick = (element, index) => {
     let currentExpenses = [...expenses];
     currentExpenses.splice(index, 1);
     setExpenses(currentExpenses)
-  }
+  };
   const handleExpensesChange = (element, index, event) => {
     let currentExpenses = [...expenses]
-    currentExpenses[index] = {...currentExpenses[index], [event.target.name]: event.target.value}
+    currentExpenses[index] = { ...currentExpenses[index], [event.target.name]: event.target.value }
     setExpenses(currentExpenses)
-  }
+  };
   //-------------------- Stepper Functionality ------------------------->
   const getStepContent = (step) => {
     switch (step) {
       case 0:
-        return(<AddTransaction 
-                  transactions={incomes}
-                  addClick={addIncomesClick}
-                  removeClick={removeIncomesClick}
-                  handleChange={handleIncomesChange}
-                  type={"Income"}
-                />);
+        return (<AddTransaction
+          transactions={incomes}
+          addClick={addIncomesClick}
+          removeClick={removeIncomesClick}
+          handleChange={handleIncomesChange}
+          type={"Income"}
+        />);
       case 1:
-        return(<AddTransaction 
-                transactions={expenses}
-                addClick={addExpensesClick}
-                removeClick={removeExpensesClick}
-                handleChange={handleExpensesChange}
-                type={"Expense"}
-              />);
+        return (<AddTransaction
+                  transactions={expenses}
+                  addClick={addExpensesClick}
+                  removeClick={removeExpensesClick}
+                  handleChange={handleExpensesChange}
+                  type={"Expense"}
+                />);
       case 2:
-        return(<Typography className={classes.instructions}>
-                  All steps completed - click finish to continue!
-               </Typography>);
+        return (<Typography className={classes.instructions}>
+          All steps completed - click finish to continue!
+        </Typography>);
       default:
         return 'Unknown step';
     }
-  }
+  };
   const [activeStep, setActiveStep] = useState(0);
   const steps = getSteps();
   const handleNext = () => {
@@ -128,82 +124,78 @@ export default function Configure(props) {
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-  const handleReset = () => {
-    setActiveStep(0);
-  };
   //------------------------------------------------------------------->
   const handleFinishAndRedirect = () => {
     axios.post('/configure', { incomes, expenses })
-    .then((response) => {
-      if(response.data.changesConfirmed) {
-        props.history.push({
-          pathname: '/index',
-          //search: '?query=abc',p
-          state: { incomes, expenses }
+      .then((response) => {
+        if (response.data.changesConfirmed) {
+          props.history.push({
+            pathname: '/index',
+            //search: '?query=abc',p
+            state: { incomes, expenses }
+          })
+        } else {
+          alert("Configure failed, " + response.data.message);
+        }
+      },
+        error => {
+          console.log("configure error");
         })
-      } else {
-        alert("Configure failed, " + response.data.message);
-      }}, 
-    error => {
-      console.log("configure error");
-})
-  }
+  };
   return (
     <Container component="main" maxWidth="lg">
-    <CssBaseline />
-    <div className={classes.root}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      <div>
-      {activeStep === steps.length ? (
-          <div>
-          {handleFinishAndRedirect()}
-          </div>
-        ) : (
-          <div>
-          <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justify="center"
-            style={{ minHeight: '80vh' }}
-          >
-            <Grid item maxWidth="sm">
-            {getStepContent(activeStep)}
-            </Grid>   
-          </Grid> 
-            <div className={classes.buttonRow}>
-              <Button 
-              variant="contained"
-              color="default"
-              disabled={activeStep === 0} 
-              onClick={handleBack} 
-              className={classes.button}>
-                Back
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNext}
-                className={classes.button}
-              >
-                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-              </Button>
-            </div>
-          </div>
-        )}
+      <CssBaseline />
+      <div className={classes.root}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        <div>
+          {activeStep === steps.length
+            ? (<div>
+                {handleFinishAndRedirect()}
+              </div>)
+            : (<div>
+                <Grid
+                  container
+                  spacing={0}
+                  direction="column"
+                  alignItems="center"
+                  justify="center"
+                  style={{ minHeight: '80vh' }}
+                >
+                  <Grid item maxWidth="sm">
+                    {getStepContent(activeStep)}
+                  </Grid>
+                </Grid>
+                <div className={classes.buttonRow}>
+                  <Button
+                    variant="contained"
+                    color="default"
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.button}>
+                      Back
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </div>
+              </div>)}
+        </div>
       </div>
-    </div>
     </Container>
   );
-}
+};
